622ee9ab3b886e77cae41ae89ad6252138ab3617
Evan Hopper-Moore <evanhoppemoore@gmail.com>
2017-09-18 19:45:57 +0000

    Cleanup merge tags

--- src/app.py
+++ src/app.py
@@ -12,7 +12,6 @@ class BuzzfeedSimulator:
         self.args = args
 
     def run(self):
-<<<<<<< HEAD
         mc = MarkovChain(2)
         manager = BuzzfeedManager('lol')
         training_data = manager.read_titles()
@@ -22,11 +21,4 @@ class BuzzfeedSimulator:
         
         markovOut = ' '.join(mc.generate_text(22))
         print markovOut
-=======
 
-        f = open("bible.txt", "r") #opens file with name of "test.txt"
-        mc = MarkovChain(1)
-        mc.add_string(f.read())
-        markovOut = ' '.join(mc.generate_text(50))
-        print markovOut
->>>>>>> 67af8b4c6cf52ec928923a078d471e3ffbbab785
