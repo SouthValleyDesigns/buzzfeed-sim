deb990dfd822f28d6fa71f3fabe9309037e7d89d
verycumbersome <matthewsages@gmail.com>
2017-09-19 00:11:03 -0700

    Add venv

--- server/BuzzfeedSimulatorApp/views.py
+++ server/BuzzfeedSimulatorApp/views.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 
 from .models import Article
-from src.app import BuzzfeedApp
+from src.app import BuzzfeedSimulator
 from django.shortcuts import render
+
+app = BuzzfeedSimulator()
 
 def index(request):
 
+    
     return render(request, 'index.html', {})
